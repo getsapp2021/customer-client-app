47b0d412b9387bb90db7aaf11d33ed435bbf4c7f
Pruthvi Patel <pruthvipatel145@gmail.com>
2021-03-10 22:18:22 +0530

    initial commit
    - Created flutter base app
    - Removed the counter home page and add the simple scaffold to display welcome messaged.
    - removed debug banner.

---
+++ android/app/src/main/kotlin/co/getsapp/customer/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.getsapp.customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gets Customer App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Gets customer app")),
+        body: Center(
+          child: Text("Welcome to gets customer app"),
+        ),
+      ),
+    );
+  }
+}
