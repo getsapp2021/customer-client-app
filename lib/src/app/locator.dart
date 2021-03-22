d4b1625ff747596c4b1f4b1fee559a0c82c88344
Pruthvi Patel <pruthvipatel145@gmail.com>
2021-03-22 13:31:04 +0530

    resolved conflicting issues

--- lib/src/app/locator.dart
+++ lib/src/app/locator.dart
@@ -1,6 +1,9 @@
 import 'package:customer/src/core/services/authentication_service.dart';
+import 'package:customer/src/core/services/firestore_service.dart';
 import 'package:customer/src/core/services/user_profile_service.dart';
+import 'package:customer/src/core/utils/g_log_printer.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:customer/src/core/services/permission_service.dart';
 
@@ -10,8 +13,11 @@ void setupLocator() {
   locator.registerLazySingleton(() => UserProfileService());
   locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => PermissionService());
+  locator.registerLazySingleton(() => FirestoreService());
 
   // Third Party Services
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => DialogService());
+
+  locator.registerLazySingleton(() => Logger(printer: GLogPrinter()));
 }
--- lib/src/ui/views/pages/auth/signin_page.dart
+++ lib/src/ui/views/pages/auth/signin_page.dart
@@ -1,20 +1,20 @@
 import 'package:customer/src/ui/utils/colors.dart';
+import 'package:customer/src/ui/utils/validators/auth_validator.dart';
 import 'package:customer/src/ui/views/pages/auth/signin_viewmodel.dart';
 import 'package:customer/src/ui/views/widgets/g_rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:stacked/stacked.dart';
 import 'package:velocity_x/velocity_x.dart';
-import 'package:customer/src/app/router.gr.dart';
 
 class SigninPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SigninViewModel>.reactive(
+    return ViewModelBuilder<SigninViewModel>.nonReactive(
       viewModelBuilder: () => SigninViewModel(),
       builder: (context, model, child) => Scaffold(
-          body: SingleChildScrollView(
-        child: Container(
+        body: SingleChildScrollView(
+          child: Container(
             padding: EdgeInsets.all(20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -24,46 +24,35 @@ class SigninPage extends StatelessWidget {
                 SizedBox(height: 20),
                 "Hey There!".text.xl3.bold.make(),
                 "Login with mobile number".text.xl.make(),
-                SizedBox(height: 20),
-                SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                          flex: 1,
-                          child: Container(
-                            child: IntlPhoneField(
-                              showDropdownIcon: false,
-                              decoration: InputDecoration(
-                                  hintText: "99999 99999",
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(50))),
-                              initialCountryCode: 'IN',
-                              onChanged: (phone) {
-                                print(phone.completeNumber);
-                              },
-                            ),
-                          )),
-                      SizedBox(
-                        width: 10.0,
-                      ),
 
-                      // ),
-                    ],
+                Form(
+                  key: model.phoneNumberFormKey,
+                  child: IntlPhoneField(
+                    autoValidate: false,
+                    validator: AuthValidator.phoneValidator,
+                    showDropdownIcon: false,
+                    maxLength: 10,
+                    decoration: InputDecoration(
+                      contentPadding: Vx.m12,
+                      hintText: "99999 99999",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    initialCountryCode: 'IN',
+                    onSaved: (phone) => model.phoneNumber = phone,
                   ),
                 ),
-                SizedBox(
-                  height: 15,
-                ),
+
                 SizedBox(height: 15),
-                GRoundedButton(
-                  onPressed: () =>
-                      model.navigateTo(Routes.accessLocationPermissionPage),
-                  color: ThemeColors.primary,
-                  textColor: ThemeColors.white,
-                  text: "Sign In",
-                ),
+                SizedBox(height: 15),
+                model.isBusy
+                    ? CircularProgressIndicator()
+                    : GRoundedButton(
+                        onPressed: () => model.performSendOtp(),
+                        color: ThemeColors.primary,
+                        text: "Sign In",
+                      ),
                 SizedBox(height: 25),
                 Center(
                   child:
@@ -73,8 +62,10 @@ class SigninPage extends StatelessWidget {
                           .make(),
                 ),
               ],
-            )),
-      )),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
